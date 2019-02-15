9b5ae81a785d05424d81a7f832ec9592481cee67
ryanbeaudet <46537503+ryanbeaudet@users.noreply.github.com>
2019-02-15 00:08:05 -0500

    Update monthly_sales.py

--- monthly_sales.py
+++ monthly_sales.py
@@ -4,20 +4,36 @@
 
 # TODO: write some Python code here to produce the desired functionality...
 
-print("-----------------------)
+#adapted from sales_reporting_exercise
+import os
+import csv
+import pandas as pd
+
+
+
+#adapted from https://www.pythonforbeginners.com/basics/getting-user-input-from-the-keyboard
+file_name = input("Please input the file you would like to be read:")
+
+sales = pd.read(file_name)
+
+print("-----------------------")
 print("MONTH: March 2018")
 
-print("-----------------------)
+
+
+print("-----------------------")
 print("CRUNCHING THE DATA...")
 
-print("-----------------------)
+total_sales = sales["sales price"].sum()
+
+print("-----------------------")
 print("TOTAL MONTHLY SALES: $12,000.71")
 
-print("-----------------------)
+print("-----------------------")
 print("TOP SELLING PRODUCTS:")
 print("  1) Button-Down Shirt: $6,960.35")
 print("  2) Super Soft Hoodie: $1,875.00")
 print("  3) etc.")
 
-print("-----------------------)
+print("-----------------------")
 print("VISUALIZING THE DATA...")
