6acd3440e7d27d343a70789eff85e35abecb23ce
ryanbeaudet <46537503+ryanbeaudet@users.noreply.github.com>
2019-02-15 00:58:12 -0500

    Validation updates

--- monthly_sales.py
+++ monthly_sales.py
@@ -14,7 +14,21 @@ import pandas as pd
 #adapted from https://www.pythonforbeginners.com/basics/getting-user-input-from-the-keyboard
 file_name = input("Please input the file you would like to be read:")
 
-sales = pd.read(file_name)
+#adapted from https://stackabuse.com/python-check-if-a-file-or-directory-exists/
+if (os.path.isfile(file_name) == False):
+    print("BAD")
+    exit()
+
+sales = pd.read_csv(file_name)
+
+#adapted from https://www.google.com/search?q=how+to+cause+a+python+program+to+stop+running&oq=how+to+cause+a+python+program+to+stop+running&aqs=chrome..69i57.8490j0j7&sourceid=chrome&ie=UTF-8
+#try:
+    #with open(file_name) as file:
+        
+#except:
+    #"Sorry! That file path does not exist."
+
+
 
 print("-----------------------")
 print("MONTH: March 2018")
@@ -24,7 +38,9 @@ print("MONTH: March 2018")
 print("-----------------------")
 print("CRUNCHING THE DATA...")
 
+#adapted from https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/exercises/sales-reporting/pandas_explore.py
 total_sales = sales["sales price"].sum()
+print(total_sales)
 
 print("-----------------------")
 print("TOTAL MONTHLY SALES: $12,000.71")
